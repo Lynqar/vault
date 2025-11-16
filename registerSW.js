5ebbd1cac6ac98d584f59fb1ab939e887a56f454
Lynqar <Lynqar@users.noreply.github.com>
2025-11-16 07:37:37 +0000

    deploy: 4e368e45cd85eaca6be525529e7913c5c79ec7cd

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/vault/sw.js', { scope: '/vault/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
